234f2ae7ce76feac9c224d335b960867e5fe2745
lvpeng <penglvc@isoftstone@.com>
2019-02-01 11:08:45 +0800

    去掉多余代码

--- src/main/java/com/server/edu/election/dao/ElectionRuleDao.java
+++ src/main/java/com/server/edu/election/dao/ElectionRuleDao.java
@@ -1,13 +1,9 @@
 package com.server.edu.election.dao;
 
 import java.util.List;
-import com.server.edu.election.dto.ElectionRuleDto;
 import com.server.edu.election.entity.ElectionRule;
-import com.server.edu.election.vo.ElectionRuleVo;
 import tk.mybatis.mapper.common.Mapper;
 import org.apache.ibatis.annotations.Param;
-import org.apache.ibatis.annotations.ResultMap;
-import org.apache.ibatis.annotations.Select;
 
 public interface ElectionRuleDao extends Mapper<ElectionRule>
 {
