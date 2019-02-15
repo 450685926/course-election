d10b045330e8ca7e2708aa599cafb4e3b0763bb9
lvpeng <penglvc@isoftstone@.com>
2019-02-15 12:54:09 +0800

    修改传参类型

--- src/main/java/com/server/edu/election/controller/ElectionConstantsController.java
+++ src/main/java/com/server/edu/election/controller/ElectionConstantsController.java
@@ -7,6 +7,7 @@ import org.apache.servicecomb.provider.rest.common.RestSchema;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -94,7 +95,7 @@ public class ElectionConstantsController {
      * @see [类、类#方法、类#成员]
      */
     @ApiOperation(value = "查看选课常量")
-    @PostMapping("/getConstants")
+    @GetMapping("/getConstants")
     public RestResult<ElectionConstants> getConstants(
     		@RequestParam("id") @NotNull Long id)
         throws Exception
