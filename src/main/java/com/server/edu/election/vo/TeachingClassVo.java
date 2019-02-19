7f2ef3215216a9e39e61c2302ffb6a4245700802
gdouyang <gdouyanga@isoftstone.com>
2019-02-19 16:27:34 +0800

    修改VO继承

--- src/main/java/com/server/edu/election/vo/TeachingClassVo.java
+++ src/main/java/com/server/edu/election/vo/TeachingClassVo.java
@@ -2,16 +2,19 @@ package com.server.edu.election.vo;
 
 import com.server.edu.dictionary.DictTypeEnum;
 import com.server.edu.dictionary.annotation.Code2Text;
+import com.server.edu.election.entity.TeachingClass;
 
-public class TeachingClassVo
+public class TeachingClassVo extends TeachingClass
 {
     @Code2Text(value=DictTypeEnum.X_XQ)
     private String campus;
     private String courseName;
+    @Override
     public String getCampus()
     {
         return campus;
     }
+    @Override
     public void setCampus(String campus)
     {
         this.campus = campus;
