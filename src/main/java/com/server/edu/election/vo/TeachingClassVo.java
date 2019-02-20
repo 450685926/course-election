441145b29aa84f22d3476f8faf537c9e411f14fc
gdouyang <gdouyanga@isoftstone.com>
2019-02-20 11:04:21 +0800

    查询类中没有 campus查询报错

--- src/main/java/com/server/edu/election/vo/TeachingClassVo.java
+++ src/main/java/com/server/edu/election/vo/TeachingClassVo.java
@@ -1,24 +1,10 @@
 package com.server.edu.election.vo;
 
-import com.server.edu.dictionary.DictTypeEnum;
-import com.server.edu.dictionary.annotation.Code2Text;
 import com.server.edu.election.entity.TeachingClass;
 
 public class TeachingClassVo extends TeachingClass
 {
-    @Code2Text(value=DictTypeEnum.X_XQ)
-    private String campus;
     private String courseName;
-    @Override
-    public String getCampus()
-    {
-        return campus;
-    }
-    @Override
-    public void setCampus(String campus)
-    {
-        this.campus = campus;
-    }
     public String getCourseName()
     {
         return courseName;
