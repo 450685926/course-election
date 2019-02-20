9dbc6548d97369ac97410bb5dd1d3f41b1e28c0f
gdouyang <gdouyanga@isoftstone.com>
2019-02-20 15:19:22 +0800

    统计筛选

--- src/main/java/com/server/edu/election/vo/TeachingClassVo.java
+++ src/main/java/com/server/edu/election/vo/TeachingClassVo.java
@@ -4,7 +4,18 @@ import com.server.edu.election.entity.TeachingClass;
 
 public class TeachingClassVo extends TeachingClass
 {
+    private String courseCode;
     private String courseName;
+    private Double credits;
+    
+    public String getCourseCode()
+    {
+        return courseCode;
+    }
+    public void setCourseCode(String courseCode)
+    {
+        this.courseCode = courseCode;
+    }
     public String getCourseName()
     {
         return courseName;
@@ -13,6 +24,13 @@ public class TeachingClassVo extends TeachingClass
     {
         this.courseName = courseName;
     }
-    
+    public Double getCredits()
+    {
+        return credits;
+    }
+    public void setCredits(Double credits)
+    {
+        this.credits = credits;
+    }
     
 }
