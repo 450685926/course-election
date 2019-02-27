29cb203aac66026e9137d5e5dfb13c6bd0341c01
bear_xiong <xzq@163.com>
2019-02-27 22:06:10 +0800

    结业生导入

--- src/main/java/com/server/edu/election/controller/OverseasAndGraduatesController.java
+++ src/main/java/com/server/edu/election/controller/OverseasAndGraduatesController.java
@@ -3,18 +3,31 @@ package com.server.edu.election.controller;
 import com.server.edu.common.PageCondition;
 import com.server.edu.common.rest.PageResult;
 import com.server.edu.common.rest.RestResult;
+
+import com.server.edu.election.entity.ElcNoGraduateStds;
 import com.server.edu.election.service.ElcNoGraduateStdsService;
 import com.server.edu.election.vo.ElcNoGraduateStdsVo;
 import com.server.edu.util.CollectionUtil;
-import io.swagger.annotations.ApiOperation;
-import io.swagger.annotations.Info;
-import io.swagger.annotations.SwaggerDefinition;
-import org.apache.servicecomb.provider.rest.common.RestSchema;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
+import com.server.edu.util.excel.GeneralExcelUtil;
+import com.server.edu.util.excel.parse.ExcelParseConfig;
+import com.server.edu.util.excel.parse.ExcelParseDesigner;
+import io.swagger.annotations.*;
 
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.servicecomb.provider.rest.common.RestSchema;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.validation.constraints.NotNull;
+import java.io.File;
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -28,6 +41,8 @@ import java.util.List;
 @RequestMapping("/overseasOrGraduate")
 public class OverseasAndGraduatesController {
 
+    static Logger logger =
+            LoggerFactory.getLogger(OverseasAndGraduatesController.class);
     @Autowired
     private ElcNoGraduateStdsService stdsService;
 
@@ -62,5 +77,55 @@ public class OverseasAndGraduatesController {
         return RestResult.success(s);
     }
 
+    @PostMapping(value = "/upload")
+    public RestResult<?> upload(@RequestPart(name = "file") MultipartFile file,
+                                @RequestPart(name = "mode") @NotNull Integer mode)
+    {
+        if (file == null)
+        {
+            return RestResult.error("文件不能为空");
+        }
 
+        String originalFilename = file.getOriginalFilename();
+        if (!originalFilename.endsWith(".xls"))
+        {
+            return RestResult.error("请使用1999-2003(.xls)类型的Excle");
+        }
+
+        try (HSSFWorkbook workbook = new HSSFWorkbook(file.getInputStream()))
+        {
+            ExcelParseDesigner designer = new ExcelParseDesigner();
+            designer.setDataStartRowIdx(1);
+            designer.setConfigs(new ArrayList<>());
+
+            designer.getConfigs().add(new ExcelParseConfig("studentId", 0));
+            designer.getConfigs().add(new ExcelParseConfig("graduateYear", 1));
+            designer.getConfigs().add(new ExcelParseConfig("remark", 2));
+            List<ElcNoGraduateStds> datas = GeneralExcelUtil
+                    .parseExcel(workbook, designer, ElcNoGraduateStds.class);
+
+            String msg =stdsService.addExcel(datas,mode);
+            return RestResult.success(msg);
+
+        }
+        catch (Exception e)
+        {
+            logger.error(e.getMessage(), e);
+            return RestResult.error("解析文件错误" + e.getMessage());
+        }
+    }
+
+    @ApiResponses({
+            @ApiResponse(code = 200, response = File.class, message = "下载模版")})
+    @GetMapping(value = "/download")
+    public ResponseEntity<Resource> download()
+    {
+        Resource resource = new ClassPathResource("/excel/jieYeShengMingDan.xls");
+
+        return ResponseEntity.ok()
+                .header(HttpHeaders.CONTENT_TYPE, "application/vnd.ms-excel")
+                .header(HttpHeaders.CONTENT_DISPOSITION,
+                        "attachment;filename=" + "jieYeShengMingDan.xls")
+                .body(resource);
+    }
 }
--- src/main/java/com/server/edu/election/service/impl/ElcNoGraduateStdsServiceImpl.java
+++ src/main/java/com/server/edu/election/service/impl/ElcNoGraduateStdsServiceImpl.java
@@ -20,6 +20,7 @@ import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * @description: 留学生
@@ -42,6 +43,7 @@ public class ElcNoGraduateStdsServiceImpl implements ElcNoGraduateStdsService {
 
     @Autowired
     private ElcCourseTakeDao takeDao;
+    
     
     /**
     *@Description: 查询留学生结业名单
@@ -115,6 +117,45 @@ public class ElcNoGraduateStdsServiceImpl implements ElcNoGraduateStdsService {
         //删除该学生选择课程
         takeDao.deleteStudentById(ids);*/
         return "common.deleteSuccess";
+    }
+
+    /**批量导入结业生*/
+    @Override
+    public String addExcel(List<ElcNoGraduateStds> datas, Integer mode) {
+        StringBuilder sb = new StringBuilder();
+        ElcNoGraduateStdsVo vo=new ElcNoGraduateStdsVo();
+        List<String> stringList  =new ArrayList<>();
+        vo.setMode(mode);
+        Page<ElcNoGraduateStdsVo> overseasOrGraduate = noGraduateStdsDao.findOverseasOrGraduate(vo);//查询所有结业生
+        if(overseasOrGraduate!=null){
+            List<ElcNoGraduateStdsVo> result = overseasOrGraduate.getResult();
+             stringList = result.stream().map(ElcNoGraduateStdsVo::getStudentId).collect(Collectors.toList());
+        }
+        for (ElcNoGraduateStds data : datas) {
+            String studentId = StringUtils.trim(data.getStudentId());
+            if(StringUtils.isNotBlank(studentId)){
+                 Student studentByCode = studentDao.findStudentByCode(studentId);
+                 if(studentByCode!=null){//学号存在
+                    if(mode==3&&"0".equals(studentByCode.getIsOverseas())&&!stringList.contains(studentId)){
+                        //插入
+                        noGraduateStdsDao.insertSelective(data);
+                    }else if(mode==4&&"1".equals(studentByCode.getIsOverseas())&&!stringList.contains(studentId)){
+                        //插入
+                        noGraduateStdsDao.insertSelective(data);
+                    }else{//学号已经存在或与是否留学不匹配
+                        sb.append(studentId+"学号存在或与是否留学不匹配");
+                    }
+                 }else{//学号不存在
+                        sb.append(studentId+"学号不存在");
+                 }
+             }
+
+        }
+
+        if(sb.length() > 0){
+            return sb.toString();
+        }
+        return StringUtils.EMPTY;
     }
 
 }
