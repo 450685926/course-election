3d205a38509ca0d0f16e0599aa9fbcb9930043f8
lvpeng <penglvc@isoftstone@.com>
2019-03-02 15:10:34 +0800

    选课数据

--- src/main/java/com/server/edu/election/studentelec/preload/CourseGradeLoad.java
+++ src/main/java/com/server/edu/election/studentelec/preload/CourseGradeLoad.java
@@ -102,7 +102,7 @@ public class CourseGradeLoad extends DataProLoad
         map.put("studentId", studentInfo.getStudentId());
         map.put("calendarId", calendarId);
         List<SelectedCourse> list = elcCourseTakeDao.findSelectedCourses(map);
-        
+        if()
         elcCourseTake.forEach(c->{
         	SelectedCourse selectedCourse = new SelectedCourse();
         	selectedCourse.setSelectedRound(c.getTurn());
