809fc1735831d1263153048d124436f38bf3248c
lvpeng <penglvc@isoftstone@.com>
2019-03-02 15:19:22 +0800

    删掉重复类

--- src/main/java/com/server/edu/election/dao/ElcCourseTakesDao.java
+++
@@ -1,7 +0,0 @@
-package com.server.edu.election.dao;
-
-import com.server.edu.election.entity.ElcCourseTakes;
-import tk.mybatis.mapper.common.Mapper;
-
-public interface ElcCourseTakesDao extends Mapper<ElcCourseTakes> {
-}
