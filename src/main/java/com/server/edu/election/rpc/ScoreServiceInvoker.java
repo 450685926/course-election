12ec63c0be34d3655715ef5e0976c1149babd20b
bear_xiong <xzq@163.com>
2019-03-04 18:48:54 +0800

    成绩调用

---
+++ src/main/java/com/server/edu/election/rpc/ScoreServiceInvoker.java
@@ -0,0 +1,20 @@
+package com.server.edu.election.rpc;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * 成绩模块微服务调用
+ *
+ * @author OuYangGuoDong
+ * @version [版本号, 2018年12月5日]
+ * @see [相关类/方法]
+ * @since [产品/模块版本]
+ */
+public class ScoreServiceInvoker {
+    private static Logger LOG =
+            LoggerFactory.getLogger(ScoreServiceInvoker.class);
+
+
+
+}
