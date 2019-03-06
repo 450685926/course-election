5e01e0509fcfdc21ac18c5a95967c4bde6305e62
bear_xiong <xzq@163.com>
2019-03-06 10:50:32 +0800

    预警学生不能选课

--- src/main/java/com/server/edu/election/studentelec/rules/bk/LoserNotElcRule.java
+++ src/main/java/com/server/edu/election/studentelec/rules/bk/LoserNotElcRule.java
@@ -1,5 +1,11 @@
 package com.server.edu.election.studentelec.rules.bk;
 
+import com.server.edu.common.locale.I18nUtil;
+import com.server.edu.election.dao.StudentDao;
+import com.server.edu.election.entity.Student;
+import com.server.edu.election.studentelec.context.ElecRespose;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import com.server.edu.election.studentelec.context.ElecContext;
@@ -14,33 +20,22 @@ import com.server.edu.election.studentelec.rules.AbstractRuleExceutor;
 public class LoserNotElcRule extends AbstractRuleExceutor {
 	private static final String PARAM = "IS_LOSER";
 
+	@Autowired
+	private StudentDao studentDao;
+
 	@Override
 	public boolean checkRule(ElecContext context, TeachingClassCache courseClass) {
-//        ElectionCourseContext electContext = (ElectionCourseContext)context;
-//        
-//        Boolean isLoser =
-//            (Boolean)electContext.getState().getParams().get("IS_LOSER");
-//        if (Boolean.TRUE.equals(isLoser))
-//        {
-//            return isLoser.booleanValue();
-//        }
-//        Student std = electContext.getStudent();
-//        OqlBuilder<LoserStudent> query =
-//            OqlBuilder.from(LoserStudent.class, "loser");
-//        query.where("loser.std.id = :stdId", std.getId())
-//            .where("loser.semester.id = :semesterId",
-//                electContext.getState().getSemesterId());
-//        
-//        LoserStudent loserStudent =
-//            (LoserStudent)this.entityDao.uniqueResult(query);
-//        isLoser = Boolean.valueOf(loserStudent != null);
-//        electContext.getState().getParams().put("IS_LOSER", isLoser);
-//        if (isLoser.booleanValue())
-//        {
-//            context.addMessage(new ElectMessage("您的不及格课程过多，不能选课",
-//                ElectRuleType.GENERAL, false, null));
-//        }
-//        return !isLoser.booleanValue();
+		if(context.getRoundId()!=null){
+			String studentId = context.getStudentInfo().getStudentId();
+			Student stu = studentDao.isLoserStu(context.getRoundId(), studentId);
+			if(stu==null){
+				return true;
+			}else{
+				ElecRespose respose = context.getRespose();
+				respose.getFailedReasons().put(courseClass.getTeacherClassId().toString(),
+						I18nUtil.getMsg("ruleCheck.isLoserStu"));
+			}
+		}
 		return false;
 	}
 }
