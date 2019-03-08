71de3a403fa1c3b591c9b6e76ec67443aa360765
bear_xiong <xzq@163.com>
2019-03-08 18:35:24 +0800

    实践课规则

--- src/main/java/com/server/edu/election/studentelec/rules/bk/SJTermCheckerRule.java
+++ src/main/java/com/server/edu/election/studentelec/rules/bk/SJTermCheckerRule.java
@@ -7,6 +7,7 @@ import com.server.edu.election.entity.ElectionRounds;
 import com.server.edu.election.rpc.BaseresServiceInvoker;
 import com.server.edu.election.rpc.CultureSerivceInvoker;
 import com.server.edu.election.studentelec.cache.StudentInfoCache;
+import com.server.edu.util.CollectionUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -15,6 +16,7 @@ import com.server.edu.election.studentelec.cache.TeachingClassCache;
 import com.server.edu.election.studentelec.rules.AbstractRuleExceutor;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * 控制实践课学期
@@ -31,6 +33,7 @@ public class SJTermCheckerRule extends AbstractRuleExceutor {
 	@Override
 	public boolean checkRule(ElecContext context, TeachingClassCache courseClass) {
 		StudentInfoCache studentInfo = context.getStudentInfo();
+		String courseCode = courseClass.getCourseCode();
 		//得到校历id
 		ElectionRounds electionRounds =
 				roundsDao.selectByPrimaryKey(context.getRoundId());
@@ -52,12 +55,25 @@ public class SJTermCheckerRule extends AbstractRuleExceutor {
 			if(i<1){
 				return false;
 			}
-			semester=String.valueOf(i);
+			semester+=String.valueOf(i);
 		}
 		List<PlanCourseTypeDto> courseType = CultureSerivceInvoker.findCourseType(studentInfo.getStudentId());
+		if(CollectionUtil.isNotEmpty(courseType)){
+			List<PlanCourseTypeDto> collect = courseType.stream().filter(temp -> temp.getWeekType().intValue() == 1).collect(Collectors.toList());
+			if(CollectionUtil.isNotEmpty(collect)){
+				for (PlanCourseTypeDto planCourseTypeDto : collect) {
+					String code = planCourseTypeDto.getCourseCode();
+					String semes = planCourseTypeDto.getSemester();
+					int i = semes.indexOf(semester);//是否有该学期
+					if(courseCode.equals(code)&& i>=0){
+						return true;
+					}
+				}
+			}
+		}
 
 
-		return true;
+		return false;
 	}
 
 
