d73d74c7d5678626689b0954f505786496bf211a
bear_xiong <xzq@163.com>
2019-03-20 16:44:07 +0800

    选课轮次规则

--- src/main/java/com/server/edu/election/studentelec/rules/bk/SecondRdCantElect1stRdFullRule.java
+++ src/main/java/com/server/edu/election/studentelec/rules/bk/SecondRdCantElect1stRdFullRule.java
@@ -1,5 +1,10 @@
 package com.server.edu.election.studentelec.rules.bk;
 
+import com.server.edu.common.locale.I18nUtil;
+import com.server.edu.election.entity.ElectionRounds;
+import com.server.edu.election.studentelec.context.ElecRespose;
+import com.server.edu.election.studentelec.service.impl.RoundDataProvider;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import com.server.edu.election.studentelec.cache.TeachingClassCache;
@@ -15,7 +20,9 @@ import com.server.edu.election.studentelec.rules.AbstractElecRuleExceutor;
 @Component("SecondRdCantElect1stRdFullRule")
 public class SecondRdCantElect1stRdFullRule extends AbstractElecRuleExceutor
 {
-    
+    @Autowired
+    private RoundDataProvider dataProvider;
+
     @Override
     public int getOrder()
     {
@@ -26,7 +33,26 @@ public class SecondRdCantElect1stRdFullRule extends AbstractElecRuleExceutor
     public boolean checkRule(ElecContext context,
         TeachingClassCache courseClass)
     {
-        
+        ElectionRounds round = dataProvider.getRound(context.getRequest().getRoundId());
+
+        if (round == null)
+        {
+            String msg = String.format("electionRounds not find roundId=%s",
+                    context.getRequest().getRoundId());
+            throw new RuntimeException(msg);
+        }
+        if(round.getTurn().intValue()==2){//第二轮
+            Integer maxNumber = courseClass.getMaxNumber();
+            Integer currentNumber = courseClass.getCurrentNumber();
+            if(maxNumber!=null&&currentNumber!=null&&currentNumber+1>maxNumber){
+                ElecRespose respose = context.getRespose();
+                respose.getFailedReasons()
+                        .put(courseClass.getCourseCodeAndClassCode(),
+                                I18nUtil.getMsg("ruleCheck.classNumberLimit"));
+                return false;
+            }
+        }
+
         return true;
     }
     
