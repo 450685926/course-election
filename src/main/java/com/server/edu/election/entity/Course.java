2d102c368624b7b606a1cea774679302837d7eca
lvpeng <penglvc@isoftstone@.com>
2019-04-23 10:50:57 +0800

    加入字典值

--- src/main/java/com/server/edu/election/entity/Course.java
+++ src/main/java/com/server/edu/election/entity/Course.java
@@ -8,6 +8,9 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
+import com.server.edu.dictionary.annotation.Code2Text;
+import com.server.edu.dictionary.annotation.CodeI18n;
+@CodeI18n
 @Table(name = "course_t")
 public class Course implements Serializable
 {
@@ -76,6 +79,7 @@ public class Course implements Serializable
     /**
      * 培养层次
      */
+    @Code2Text(transformer = "X_PYCC")
     @Column(name = "TRAINING_LEVEL_")
     private String trainingLevel;
     
@@ -94,6 +98,7 @@ public class Course implements Serializable
     /**
      * 所属学院
      */
+    @Code2Text(transformer="X_YX")
     @Column(name = "COLLEGE_")
     private String college;
     
