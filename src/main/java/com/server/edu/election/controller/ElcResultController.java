2f370de9e37a49d4b029574841313cee83a5f841
qiangliz <qiangliz@isoftstone.com>
2019-07-25 16:18:26 +0800

    导出修改

--- src/main/java/com/server/edu/election/controller/ElcResultController.java
+++ src/main/java/com/server/edu/election/controller/ElcResultController.java
@@ -9,6 +9,8 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -190,9 +192,9 @@ public class ElcResultController
 
     @ApiResponses({
         @ApiResponse(code = 200, response = File.class, message = "选课学生统计导出")})
-    @PostMapping("/elcResultCountByStudentExport")
+    @GetMapping("/elcResultCountByStudentExport")
     public File elcResultCountByStudentExport(
-    		@RequestBody ElcResultQuery condition)
+    		@ModelAttribute ElcResultQuery condition)
     {
     	try {
             RestResult<String> restResult = elcResultService.elcResultCountsExport(condition);
@@ -232,9 +234,9 @@ public class ElcResultController
     
     @ApiResponses({
         @ApiResponse(code = 200, response = File.class, message = "未选课学生名单导出")})
-    @PostMapping("/export")
+    @GetMapping("/export")
     public File export(
-    		@RequestBody ElcResultQuery condition)
+    		@ModelAttribute ElcResultQuery condition)
     {
     	try {
             RestResult<String> restResult = elcResultService.exportOfNonSelectedCourse(condition);
