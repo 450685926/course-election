8694e8805ff192c4c220893547256e257d8285d3
gdouyang <gdouyanga@isoftstone.com>
2019-08-02 11:24:35 +0800

    test

--- src/main/java/com/server/edu/election/controller/TestController.java
+++ src/main/java/com/server/edu/election/controller/TestController.java
@@ -2,6 +2,7 @@ package com.server.edu.election.controller;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 import javax.validation.constraints.NotNull;
 
@@ -10,13 +11,15 @@ import org.hibernate.validator.constraints.NotBlank;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import com.alibaba.fastjson.JSON;
 import com.server.edu.common.rest.RestResult;
 import com.server.edu.election.entity.ElectionRounds;
-import com.server.edu.election.studentelec.service.StudentElecService;
 import com.server.edu.election.studentelec.service.impl.RoundDataProvider;
 import com.server.edu.election.vo.ElectionRoundsVo;
 import com.server.edu.exception.ParameterValidateException;
@@ -32,7 +35,7 @@ public class TestController
     Logger LOG = LoggerFactory.getLogger(TestController.class);
     
     @Autowired
-    private StudentElecService elecService;
+    private StringRedisTemplate redisTemplate;
     
     @Autowired
     private RoundDataProvider dataProvider;
@@ -50,16 +53,24 @@ public class TestController
         return RestResult.successData(data);
     }
     
-    @PostMapping("/getRounds1")
-    public RestResult<List<ElectionRoundsVo>> getRounds1(
+    @PostMapping("/getRound2")
+    public RestResult<List<ElectionRoundsVo>> getRound2(
         @RequestParam("electionObj") @NotBlank String electionObj,
         @RequestParam("projectId") @NotBlank String projectId,
         @RequestParam(name = "mode") @NotNull Integer mode,
-        @RequestParam(name = "studentId") String studentId)
-    {
-        SessionUtils.getCurrentSession();
+        @RequestParam(name = "studentId") String studentId){
+        
+        String string = redisTemplate.opsForValue().get("testElc_test1");
+        
         List<ElectionRoundsVo> data = new ArrayList<>();
         return RestResult.successData(data);
+    }
+    
+    @GetMapping("/test1")
+    public RestResult<?> test1(){
+        List<ElectionRounds> allRound = dataProvider.getAllRound();
+        redisTemplate.opsForValue().set("testElc_test1", JSON.toJSONString(allRound), 1, TimeUnit.HOURS);
+        return RestResult.success();
     }
     
     @PostMapping("/getSession")
