408d7b1436fd684880ee4dc7fbfe8a94eca0039a
xlluoc <xlluoc@isoftstone.com>
2019-08-17 15:18:48 +0800

    英语小课门数限制规则

--- src/main/java/com/server/edu/election/service/ElectionConstantsService.java
+++ src/main/java/com/server/edu/election/service/ElectionConstantsService.java
@@ -1,5 +1,7 @@
 package com.server.edu.election.service;
 
+import java.util.List;
+
 import com.github.pagehelper.PageInfo;
 import com.server.edu.common.PageCondition;
 import com.server.edu.election.dto.ElectionConstantsDto;
@@ -13,5 +15,11 @@ public interface ElectionConstantsService {
 	int update(ElectionConstantsDto dto);
 	
 	ElectionConstants getConstants(Long id);
+	
+	/**
+	 * 获取研究生常量列表
+	 * @return
+	 */
+	List<ElectionConstants> getAllGraduateConstants(String projectId);
 
 }
