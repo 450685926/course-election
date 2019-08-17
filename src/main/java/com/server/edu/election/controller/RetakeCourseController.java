ffdb561b8eee3fa29da7dfbbddb9fdbef3b194a7
gmlic <gmlic@isoftstone.com>
2019-08-17 17:24:25 +0800

    重修修改

--- src/main/java/com/server/edu/election/controller/RetakeCourseController.java
+++ src/main/java/com/server/edu/election/controller/RetakeCourseController.java
@@ -58,8 +58,9 @@ public class RetakeCourseController {
 
     @ApiOperation(value = "查询重修选课开关状态")
     @GetMapping("/getRetakeRule")
-    public RestResult<Boolean> getRetakeRule(@RequestParam("calendarId") Long calendarId, @RequestParam("projectId") String projectId) {
-        return RestResult.successData(retakeCourseService.getRetakeRule(calendarId, projectId));
+    public RestResult<Boolean> getRetakeRule(@RequestParam("calendarId") Long calendarId) {
+        Session currentSession = SessionUtils.getCurrentSession();
+        return RestResult.successData(retakeCourseService.getRetakeRule(calendarId, currentSession.getCurrentManageDptId()));
     }
 
     @ApiOperation(value = "查询重修选课门数上限列表")
