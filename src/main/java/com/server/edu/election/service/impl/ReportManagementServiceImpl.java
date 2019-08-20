90cc8c59d91b802e7c3f328bd53af3f0c9ddc790
gmlic <gmlic@isoftstone.com>
2019-08-21 00:27:39 +0800

    学生课表导出修改

--- src/main/java/com/server/edu/election/service/impl/ReportManagementServiceImpl.java
+++ src/main/java/com/server/edu/election/service/impl/ReportManagementServiceImpl.java
@@ -1211,7 +1211,7 @@ public class ReportManagementServiceImpl implements ReportManagementService
         list.add(timeTable.getAssessmentMode());
         list.add(String.valueOf(timeTable.getCredits()));
         list.add(timeTable.getTeacherName());
-        list.add(timeTable.getTime() + timeTable.getRoom());
+        list.add(timeTable.getTime());
         list.add(timeTable.getRemark());
         return list;
     }
