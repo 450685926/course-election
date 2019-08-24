660a5d4bd9fa7e0e58203f02e611113191393734
gdouyang <gdouyanga@isoftstone.com>
2019-08-24 14:39:22 +0800

    #5283 【本科生选课】【选课申请课程管理】筛选条件多字段，无搜索功能

--- src/main/java/com/server/edu/election/dto/ElectionApplyCoursesDto.java
+++ src/main/java/com/server/edu/election/dto/ElectionApplyCoursesDto.java
@@ -8,48 +8,70 @@ import org.hibernate.validator.constraints.NotEmpty;
 
 import com.server.edu.election.entity.Course;
 
-public class ElectionApplyCoursesDto extends Course {
+public class ElectionApplyCoursesDto extends Course
+{
     private static final long serialVersionUID = 1L;
+    
     @NotNull
     private Long calendarId;
+    
     @NotEmpty
     private List<String> courses;
-    private String keyCode;
+    
+    private String keyword;
+    
     private Integer weekNum;
     
     private Integer mode;
     
+    public Integer getMode()
+    {
+        return mode;
+    }
     
-	public Integer getMode() {
-		return mode;
-	}
-	public void setMode(Integer mode) {
-		this.mode = mode;
-	}
-	public Integer getWeekNum() {
-		return weekNum;
-	}
-	public void setWeekNum(Integer weekNum) {
-		this.weekNum = weekNum;
-	}
-	public String getKeyCode() {
-		return keyCode;
-	}
-	public void setKeyCode(String keyCode) {
-		this.keyCode = keyCode;
-	}
-	public Long getCalendarId() {
-		return calendarId;
-	}
-	public void setCalendarId(Long calendarId) {
-		this.calendarId = calendarId;
-	}
-	public List<String> getCourses() {
-		return courses;
-	}
-	public void setCourses(List<String> courses) {
-		this.courses = courses;
-	}
+    public void setMode(Integer mode)
+    {
+        this.mode = mode;
+    }
     
-
+    public Integer getWeekNum()
+    {
+        return weekNum;
+    }
+    
+    public void setWeekNum(Integer weekNum)
+    {
+        this.weekNum = weekNum;
+    }
+    
+    public String getKeyword()
+    {
+        return keyword;
+    }
+    
+    public void setKeyword(String keyword)
+    {
+        this.keyword = keyword;
+    }
+    
+    public Long getCalendarId()
+    {
+        return calendarId;
+    }
+    
+    public void setCalendarId(Long calendarId)
+    {
+        this.calendarId = calendarId;
+    }
+    
+    public List<String> getCourses()
+    {
+        return courses;
+    }
+    
+    public void setCourses(List<String> courses)
+    {
+        this.courses = courses;
+    }
+    
 }
