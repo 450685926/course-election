5bdcbf6d0762c150ca9fe49918c813ff8c1283f2
lvpeng <penglvc@isoftstone@.com>
2019-10-25 15:49:53 +0800

    本科生选课

--- src/main/java/com/server/edu/election/service/impl/BkStudentScoreServiceImpl.java
+++ src/main/java/com/server/edu/election/service/impl/BkStudentScoreServiceImpl.java
@@ -3,6 +3,7 @@ package com.server.edu.election.service.impl;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import com.server.edu.common.vo.SchoolCalendarVo;
 import com.server.edu.common.vo.ScoreStudentResultVo;
@@ -10,7 +11,7 @@ import com.server.edu.dictionary.utils.SchoolCalendarCacheUtil;
 import com.server.edu.election.dao.StudentUndergraduateScoreInfoDao;
 import com.server.edu.election.dto.StudentScoreDto;
 import com.server.edu.election.service.BkStudentScoreService;
-
+@Service
 public class BkStudentScoreServiceImpl implements BkStudentScoreService {
 	@Autowired
 	private StudentUndergraduateScoreInfoDao infoDao; 
