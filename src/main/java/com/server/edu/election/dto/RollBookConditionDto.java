f1184541ea8db6ae341999307e0f9f0880aea1e9
clyu <clyu@isoftstone.com>
2019-11-25 10:01:02 +0800

    报表管理:点名册导入优化

--- src/main/java/com/server/edu/election/dto/RollBookConditionDto.java
+++ src/main/java/com/server/edu/election/dto/RollBookConditionDto.java
@@ -16,9 +16,18 @@ public class RollBookConditionDto {
     private String teacherCode;
     private String courseNature;
     private String classCode;
+    private List<String> classCodeList;
     private String isElective;
     private String projectId;
     private List<Long> ids;//导出列表id集合
+
+    public List<String> getClassCodeList() {
+        return classCodeList;
+    }
+
+    public void setClassCodeList(List<String> classCodeList) {
+        this.classCodeList = classCodeList;
+    }
 
     public void setProjectId(String projectId) {
         this.projectId = projectId;
