cd5bc7d6d6ad9a7648fa961a90bfb6af9ae83372
ming <450685926@qq.com>
2019-12-04 17:22:26 +0800

    修改

--- src/main/java/com/server/edu/election/studentelec/rules/bk/LimitCountCheckerRule.java
+++ src/main/java/com/server/edu/election/studentelec/rules/bk/LimitCountCheckerRule.java
@@ -1,5 +1,8 @@
 package com.server.edu.election.studentelec.rules.bk;
 
+import com.server.edu.election.constants.Constants;
+import com.server.edu.election.entity.ElectionRounds;
+import com.server.edu.election.studentelec.context.ElecRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -35,6 +38,25 @@ public class LimitCountCheckerRule extends AbstractElecRuleExceutorBk
         Integer maxNumber = courseClass.getMaxNumber();
         Integer reserveNumber = courseClass.getReserveNumber();
         Integer currentNumber = dataProvider.getElecNumber(teachClassId);
+        ElecRequest request = context.getRequest();
+        Long roundId = request.getRoundId();
+        ElectionRounds round = dataProvider.getRound(roundId);
+        Integer chooseObj = request.getChooseObj();
+        int turn = round.getTurn().intValue();
+        // 判断是不是三四轮
+        if (chooseObj.intValue() == Constants.ONE && (turn == Constants.THIRD_TURN || turn == Constants.FOURTH_TURN )) {
+            if (maxNumber != null && currentNumber != null
+                    && currentNumber + courseClass.getThirdWithdrawNumber() + reserveNumber + 1 <= maxNumber)
+            {
+                return true;
+            }
+            ElecRespose respose = context.getRespose();
+            respose.getFailedReasons()
+                    .put(courseClass.getCourseCodeAndClassCode(),
+                            I18nUtil.getMsg("ruleCheck.limitCount"));
+
+            return false;
+        }
         if (maxNumber != null && currentNumber != null
             && currentNumber + reserveNumber + 1 <= maxNumber)
         {
