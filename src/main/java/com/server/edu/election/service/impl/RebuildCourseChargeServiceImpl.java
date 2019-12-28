fb052e5582ffce2bd2d96122d0991795ebed81fa
liqiang <qq577854218@sina.cn>
2019-12-28 15:42:28 +0800

    修改选课回收站

--- src/main/java/com/server/edu/election/service/impl/RebuildCourseChargeServiceImpl.java
+++ src/main/java/com/server/edu/election/service/impl/RebuildCourseChargeServiceImpl.java
@@ -167,7 +167,6 @@ public class RebuildCourseChargeServiceImpl implements RebuildCourseChargeServic
         rebuildCourseDto.setTurn(Integer.valueOf(String.valueOf(turn)));
         rebuildCourseDto.setElectionObj(StringUtils.isEmpty(electionObj)?null:electionObj);
         rebuildCourseDto.setDeptId(dptId);
-        pageCondition.setCondition(rebuildCourseDto);
         while (true) {
             pageNum++;
             pageCondition.setPageNum_(pageNum);
@@ -369,11 +368,14 @@ public class RebuildCourseChargeServiceImpl implements RebuildCourseChargeServic
      */
     @Override
     public PageResult<RebuildCourseNoChargeList> findRecycleCourse(PageCondition<RebuildCourseDto> condition) {
-        String dptId = SessionUtils.getCurrentSession().getCurrentManageDptId();
-        condition.getCondition().setDeptId(dptId);
+        RebuildCourseDto condition1 = condition.getCondition();
+        if (StringUtils.isEmpty(condition1.getDeptId())){
+            String dptId = SessionUtils.getCurrentSession().getCurrentManageDptId();
+            condition1.setDeptId(dptId);
+        }
         PageHelper.startPage(condition.getPageNum_(), condition.getPageSize_());
         Page<RebuildCourseNoChargeList> recycleCourse =
-                courseChargeDao.findRecycleCourse(condition.getCondition());
+                courseChargeDao.findRecycleCourse(condition1);
         return new PageResult<>(recycleCourse);
     }
 
