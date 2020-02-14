a19584af8a39f39ee71bda50305acd902c5efe6b
luoxiaoli <lightwind24@163.com>
2020-02-14 02:32:47 +0000

    Merge branch 'dev' into 'dev'
    字符串的合并处理
    
    See merge request tongji/course-election!3250
    
    (cherry picked from commit 9c507b0890e70e8b1f05089532a177666f26d66a)
    
    66e481ae 字符串的合并处理
    a423e1a7 字符串的合并处理

--- src/main/java/com/server/edu/mutual/service/impl/ElcMutualListServiceImpl.java
+++ src/main/java/com/server/edu/mutual/service/impl/ElcMutualListServiceImpl.java
@@ -19,62 +19,62 @@ import com.server.edu.session.util.entity.Session;
 import org.springframework.stereotype.Service;
 
 @Service
-public class ElcMutualListServiceImpl implements ElcMutualListService{
-	@Autowired
-	private ElcMutualListDao elcMutualListDao;
-	
-	@Override
-	public PageInfo<ElcMutualListVo> getMutualStuList(PageCondition<ElcMutualListDto> condition) {
-		PageHelper.startPage(condition.getPageNum_(), condition.getPageSize_());
-		
-		Session session = SessionUtils.getCurrentSession();
-		String faculty = session.getFaculty();
-		String projectId = session.getCurrentManageDptId();
-		
-		ElcMutualListDto dto = condition.getCondition();
-		
-    	if(Constants.BK_CROSS.equals(dto.getMode())) {
-    		dto.setInType(Constants.FIRST);
-    	}else {
-    		dto.setByType(Constants.FIRST);
-    	}
-    	
-    	// 教务员查看本学院申请了本研互选选课的学生和申请了本学院开设课程的学生
-    	boolean isAcdemicDean = StringUtils.equals(session.getCurrentRole(), String.valueOf(Constants.ONE)) && !session.isAdmin() && session.isAcdemicDean();
-    	if (isAcdemicDean) {
-    		if (dto.getProjectIds().contains(projectId)) {
-    			dto.setCollege(faculty);  // 学生行政学院
-			}else {
-				dto.setOpenCollege(faculty);  // 开课学院
-			}
-		}
-		
-    	List<ElcMutualListVo> list = elcMutualListDao.getMutualStuList(dto);
-		PageInfo<ElcMutualListVo> pageInfo = new PageInfo<ElcMutualListVo>(list);
-		return pageInfo;
-	}
+public class ElcMutualListServiceImpl implements ElcMutualListService {
+    @Autowired
+    private ElcMutualListDao elcMutualListDao;
 
-	@Override
-	public PageInfo<ElcMutualListVo> getMutualCourseList(PageCondition<ElcMutualListDto> condition) {
-		PageHelper.startPage(condition.getPageNum_(), condition.getPageSize_());
-		ElcMutualListDto dto = condition.getCondition();
-		Session session = SessionUtils.getCurrentSession();
-		String faculty = session.getFaculty();
-		
-		if(Constants.BK_CROSS.equals(dto.getMode())) {
-			dto.setInType(Constants.FIRST);
-		}else {
-			dto.setByType(Constants.FIRST);
-		}
-		
-    	boolean isAcdemicDean = StringUtils.equals(session.getCurrentRole(), String.valueOf(Constants.ONE)) && !session.isAdmin() && session.isAcdemicDean();
-		if (isAcdemicDean) {
-			dto.setOpenCollege(faculty);
-		}
-    	
-		List<ElcMutualListVo> list = elcMutualListDao.getMutualCourseList(dto);
-		PageInfo<ElcMutualListVo> pageInfo = new PageInfo<ElcMutualListVo>(list);
-		return pageInfo;
-	}
+    @Override
+    public PageInfo<ElcMutualListVo> getMutualStuList(PageCondition<ElcMutualListDto> condition) {
+        PageHelper.startPage(condition.getPageNum_(), condition.getPageSize_());
+
+        Session session = SessionUtils.getCurrentSession();
+        String faculty = session.getFaculty();
+        String projectId = session.getCurrentManageDptId();
+
+        ElcMutualListDto dto = condition.getCondition();
+
+        if (Constants.BK_CROSS.equals(dto.getMode())) {
+            dto.setInType(Constants.FIRST);
+        } else {
+            dto.setByType(Constants.FIRST);
+        }
+
+        // 教务员查看本学院申请了本研互选选课的学生和申请了本学院开设课程的学生
+        boolean isAcdemicDean = StringUtils.equals(session.getCurrentRole(), String.valueOf(Constants.ONE)) && !session.isAdmin() && session.isAcdemicDean();
+        if (isAcdemicDean) {
+            if (dto.getProjectIds().contains(projectId)) {
+                dto.setCollege(faculty);  // 学生行政学院
+            } else {
+                dto.setOpenCollege(faculty);  // 开课学院
+            }
+        }
+
+        List<ElcMutualListVo> list = elcMutualListDao.getMutualStuList(dto);
+        PageInfo<ElcMutualListVo> pageInfo = new PageInfo<ElcMutualListVo>(list);
+        return pageInfo;
+    }
+
+    @Override
+    public PageInfo<ElcMutualListVo> getMutualCourseList(PageCondition<ElcMutualListDto> condition) {
+        PageHelper.startPage(condition.getPageNum_(), condition.getPageSize_());
+        ElcMutualListDto dto = condition.getCondition();
+        Session session = SessionUtils.getCurrentSession();
+        String faculty = session.getFaculty();
+
+        if (Constants.BK_CROSS.equals(dto.getMode())) {
+            dto.setInType(Constants.FIRST);
+        } else {
+            dto.setByType(Constants.FIRST);
+        }
+
+        boolean isAcdemicDean = StringUtils.equals(session.getCurrentRole(), String.valueOf(Constants.ONE)) && !session.isAdmin() && session.isAcdemicDean();
+        if (isAcdemicDean) {
+            dto.setOpenCollege(faculty);
+        }
+
+        List<ElcMutualListVo> list = elcMutualListDao.getMutualCourseList(dto);
+        PageInfo<ElcMutualListVo> pageInfo = new PageInfo<ElcMutualListVo>(list);
+        return pageInfo;
+    }
 
 }
