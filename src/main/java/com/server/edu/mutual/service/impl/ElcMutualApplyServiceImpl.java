d627737558b16ac63021dcbe82fe2edfb54b2e44
luoxiaoli <lightwind24@163.com>
2020-03-04 09:23:36 +0000

    Merge branch 'dev' into 'dev'
    【本研互选】【研究生】【开课学院审核】开课学院审核，点击驳回之后，学生切换登陆界面，驳回的单据不能再次重新申请了
    
    See merge request tongji/course-election!3758
    
    (cherry picked from commit 1648a584c7c7ab10dbecbf6bc2a83fe5ebc87943)
    
    2dd8f5ee 【本研互选】【研究生】【开课学院审核】开课学院审核，点击驳回之后，学生切换登陆界面，驳回的单据不能再次重新申请了

--- src/main/java/com/server/edu/mutual/service/impl/ElcMutualApplyServiceImpl.java
+++ src/main/java/com/server/edu/mutual/service/impl/ElcMutualApplyServiceImpl.java
@@ -109,26 +109,26 @@ public class ElcMutualApplyServiceImpl implements ElcMutualApplyService {
 					List<Long> indexList = new ArrayList<>();
 					for (int i = 0; i < list.size(); i++) {
 						ElcMutualApplyVo listApply = list.get(i);
-						for (ElcMutualApplyVo courseApply : coursesForStuList) {
+						for(int j = 0; j< coursesForStuList.size();j++){
+							ElcMutualApplyVo courseApply = coursesForStuList.get(i);
 							if(listApply.getCourseCode().equals(courseApply.getCourseCode())
-								&& String.valueOf(listApply.getMutualCourseId()).equals(courseApply.getCourseId())
-							    && listApply.getOpenCollege().equals(courseApply.getOpenCollege())){
+									&& String.valueOf(listApply.getMutualCourseId()).equals(courseApply.getCourseId())
+									&& listApply.getOpenCollege().equals(courseApply.getOpenCollege())){
 								//将相同数据的主键放到list中
-								indexList.add(listApply.getId());
+								indexList.add(courseApply.getId());
 							}
 						}
 					}
 					//去除相同的数据
-					for (int n = 0; n < list.size(); n++) {
-						ElcMutualApplyVo elcVo = list.get(n);
+					for (int n = 0; n < coursesForStuList.size(); n++) {
+						ElcMutualApplyVo elcVo = coursesForStuList.get(n);
 						for (int m = 0; m < indexList.size(); m++) {
 							if(elcVo.getId() == indexList.get(m)){
-								list.remove(elcVo);
+								coursesForStuList.remove(elcVo);
 							}
 						}
 					}
 				}
-
 				list.addAll(coursesForStuList);
 			}
 		}
